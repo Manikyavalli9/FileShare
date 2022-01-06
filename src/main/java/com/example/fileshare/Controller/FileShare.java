615d11aab692052db8d7592a6a1693bd4f68d0fb
yanamandra.kyavalli <yanamandra.kyavalli@theleanapps.com>
2022-01-06 18:00:00 +0530

    detailed explanation on why we are using get and post mapping annotations

--- src/main/java/com/example/fileshare/Controller/FileShare.java
+++ src/main/java/com/example/fileshare/Controller/FileShare.java
@@ -6,19 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RestController
 public class FileShare {
 //linking to service of file
     @Autowired
     private FileService service;
-
+//it gets all the files
     @GetMapping("/api/file")
     public List<File> getFile() {
         return service.getFile();
     }
-
+//it shows or get specific id of the file
     @GetMapping("/api/file/{id}")
     public ResponseEntity<File> getFileById(@PathVariable(name = "id") String id) {
         return service.getFileById(id);
--- src/main/java/com/example/fileshare/Controller/User.java
+++ src/main/java/com/example/fileshare/Controller/User.java
@@ -5,12 +5,14 @@ import com.example.fileshare.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class User {
     //auto linking to user service class
     @Autowired
     public UserService userService;
-
+//post mapping for registering users.....
     @PostMapping("/register")
     public void register(@RequestBody User user) {
         userService.register(user);
